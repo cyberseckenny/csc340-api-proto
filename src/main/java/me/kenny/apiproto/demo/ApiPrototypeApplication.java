5d87d189942c045a5f77c69d1dbb29a3fb59638a
cyberseckenny <kennycs@protonmail.com>
2023-09-05 15:38:10 -0400

    Retrieve json data from weather.gov

--- src/main/java/me/kenny/apiproto/demo/ApiPrototypeApplication.java
+++ src/main/java/me/kenny/apiproto/demo/ApiPrototypeApplication.java
@@ -1,14 +1,27 @@
 package me.kenny.apiproto.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiPrototypeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPrototypeApplication.class, args);
-		System.out.println("Hello World!");
+		weather();
 	}
 
+	public static void weather() {
+		String url = "https://api.weather.gov/points/36.072701,-79.793900"; // greensboro
+		RestTemplate restTemplate = new RestTemplate();
+		ObjectMapper mapper = new ObjectMapper();
+
+		String json = restTemplate.getForObject(url, String.class);
+
+		System.out.println(json);
+	}
 }
